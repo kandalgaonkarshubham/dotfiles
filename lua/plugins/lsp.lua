fd326e72c29dd76296b536ea19a0130e78335978
kandalgaonkarshubham <kandalgaonkarshubham@gmail.com>
2025-11-07 10:17:54 +0530

    fix: sonarlint config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -24,15 +24,15 @@ return {
             "--log-level",
             "DEBUG",
           },
-        },
-        settings = {
-          sonarlint = {
-            test = "test",
-            rules = {
-              ["typescript:S101"] = { level = "on", parameters = { format = "^[A-Z][a-zA-Z0-9]*$" } },
-              ["typescript:S103"] = { level = "on", parameters = { maximumLineLength = 180 } },
-              ["typescript:S106"] = { level = "on" },
-              ["typescript:S107"] = { level = "on", parameters = { maximumFunctionParameters = 7 } },
+          settings = {
+            sonarlint = {
+              test = "test",
+              rules = {
+                ["typescript:S101"] = { level = "on", parameters = { format = "^[A-Z][a-zA-Z0-9]*$" } },
+                ["typescript:S103"] = { level = "on", parameters = { maximumLineLength = 180 } },
+                ["typescript:S106"] = { level = "on" },
+                ["typescript:S107"] = { level = "on", parameters = { maximumFunctionParameters = 7 } },
+              },
             },
           },
         },
