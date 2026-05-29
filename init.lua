75f60946042d957522c67ba334f4d144c6f40e97
kandalgaonkarshubham <kandalgaonkarshubham@gmail.com>
2026-05-29 15:58:57 +0530

    feat: add commands

--- init.lua
+++ init.lua
@@ -1,3 +1,6 @@
 -- require("vim._core.ui2").enable({})
 
-require("config")
+-- config
+require("config.options")
+require("config.keymaps")
+require("config.autocmds")
---
+++ lua/config/autocmds.lua
@@ -0,0 +1,26 @@
+--? [[ MasonUpdateAllComplete ]]
+-- vim.api.nvim_create_autocmd("User", {
+--   pattern = "MasonUpdateAllComplete",
+--   callback = function()
+--     print("mason-update-all has finished")
+--   end,
+-- })
+
+--? [[ Disable autoformat for specific directories ]]
+-- vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+--   callback = function()
+--     local cwd = vim.fn.getcwd()
+--     -- print("Current working directory: " .. cwd)
+
+--     local disabled_dirs = {
+--       "/home/tazerblaze/Projects/wntp",
+--     }
+--     for _, dir in ipairs(disabled_dirs) do
+--       if cwd:find(vim.fn.expand(dir)) == 1 then
+--         vim.b.autoformat = false
+--         -- print("Autoformat is disabled for this directory.")
+--         break
+--       end
+--     end
+--   end,
+-- })
