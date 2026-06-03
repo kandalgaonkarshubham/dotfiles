a4eac5aba6a1d9b5ca81d22a9e635507542768a5
kandalgaonkarshubham <kandalgaonkarshubham@gmail.com>
2026-06-03 09:42:35 +0530

    feat: setup noice

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -60,3 +60,6 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     vim.hl.on_yank()
   end,
 })
+
+--! [[ Plugin Options ]]
+--
---
+++ lua/plugins/ui.lua
@@ -0,0 +1,20 @@
+-- if true then return {} end --! WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+return {
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      presets = {
+        command_palette = true,
+        inc_rename = false, -- enables an input dialog for inc-rename.nvim
+        lsp_doc_border = true,
+        long_message_to_split = true,
+      },
+    },
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
+    }
+  }
+}
