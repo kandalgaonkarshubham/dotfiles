5f62e7bc5bb12057a2e73266f06e071d122c1a4d
kandalgaonkarshubham <kandalgaonkarshubham@gmail.com>
2026-06-03 22:20:29 +0530

    refactor: plugins placement

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -73,54 +73,6 @@ return {
     end
   },
   {
-    "stevearc/conform.nvim",
-    opts = {
-      formatters_by_ft = {
-        lua = { "stylua" },
-        javascript = { "prettierd" },
-        typescript = { "prettierd" },
-        javascriptreact = { "prettierd" },
-        typescriptreact = { "prettierd" },
-        php = { "prettierd" },
-        html = { "prettierd" },
-        css = { "prettierd" },
-        json = { "prettierd" },
-        vue = { "prettierd" },
-        markdown = { "prettierd" },
-      },
-    },
-  },
-  {
-    "mfussenegger/nvim-lint",
-    config = function()
-      local lint = require("lint")
-
-      lint.linters_by_ft = {
-        dotenv = { "dotenv_linter" },
-        javascript = { "eslint" },
-        typescript = { "eslint" },
-        javascriptreact = { "eslint" },
-        typescriptreact = { "eslint" },
-        html = { "eslint" },
-        css = { "eslint" },
-        json = { "eslint" },
-        vue = { "eslint" },
-        markdown = { "eslint" },
-        lua = { "eslint" },
-        php = { "eslint" },
-      }
-
-      vim.api.nvim_create_autocmd(
-        { "BufEnter", "BufWritePost", "InsertLeave" },
-        {
-          callback = function()
-            lint.try_lint()
-          end,
-        }
-      )
-    end,
-  },
-  {
     "schrieveslaach/sonarlint.nvim",
     url = "https://gitlab.com/schrieveslaach/sonarlint.nvim",
     ft = { "html", "php", "javascript", "typescript", "javascriptreact", "typescriptreact" },
@@ -167,6 +119,54 @@ return {
           -- 'java',
         },
       })
+    end,
+  },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        lua = { "stylua" },
+        javascript = { "prettierd" },
+        typescript = { "prettierd" },
+        javascriptreact = { "prettierd" },
+        typescriptreact = { "prettierd" },
+        php = { "prettierd" },
+        html = { "prettierd" },
+        css = { "prettierd" },
+        json = { "prettierd" },
+        vue = { "prettierd" },
+        markdown = { "prettierd" },
+      },
+    },
+  },
+  {
+    "mfussenegger/nvim-lint",
+    config = function()
+      local lint = require("lint")
+
+      lint.linters_by_ft = {
+        dotenv = { "dotenv_linter" },
+        javascript = { "eslint" },
+        typescript = { "eslint" },
+        javascriptreact = { "eslint" },
+        typescriptreact = { "eslint" },
+        html = { "eslint" },
+        css = { "eslint" },
+        json = { "eslint" },
+        vue = { "eslint" },
+        markdown = { "eslint" },
+        lua = { "eslint" },
+        php = { "eslint" },
+      }
+
+      vim.api.nvim_create_autocmd(
+        { "BufEnter", "BufWritePost", "InsertLeave" },
+        {
+          callback = function()
+            lint.try_lint()
+          end,
+        }
+      )
     end,
   },
 }
