ecba3d0f21a65298c3341483d15059be43cd74e6
kandalgaonkarshubham <kandalgaonkarshubham@gmail.com>
2026-06-06 16:18:50 +0530

    feat: setup gitsigns

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -17,6 +17,9 @@ vim.pack.add({
   { src = "https://github.com/nvim-mini/mini.surround" },
   { src = "https://github.com/nvim-mini/mini.extra" },
   { src = "https://github.com/nvim-mini/mini.pick" },
+
+  -- Git
+  { src = "https://github.com/lewis6991/gitsigns.nvim" },
 })
 
 --------------------------------------------------------------------------------
@@ -337,7 +340,7 @@ map("n", "<leader>ss", function()
 end, { desc = "LSP Symbols" })
 
 --------------------------------------------------------------------------------
--- mini
+-- Mini
 --------------------------------------------------------------------------------
 
 require("mini.surround").setup()
@@ -346,4 +349,86 @@ require("mini.extra").setup()
 
 map("n", "<leader>xx", MiniExtra.pickers.diagnostic, {
   desc = "Diagnostics",
+})
+
+--------------------------------------------------------------------------------
+-- Git
+--------------------------------------------------------------------------------
+
+require("gitsigns").setup({
+  current_line_blame = true,
+  on_attach = function(bufnr)
+    local gitsigns = require("gitsigns")
+
+    local function map(mode, l, r, opts)
+      opts = opts or {}
+      opts.buffer = bufnr
+      vim.keymap.set(mode, l, r, opts)
+    end
+
+    -- Navigation
+    map("n", "]c", function()
+      if vim.wo.diff then
+        vim.cmd.normal({ "]c", bang = true })
+      else
+        gitsigns.nav_hunk("next")
+      end
+    end, { desc = "Next Git hunk" })
+
+    map("n", "[c", function()
+      if vim.wo.diff then
+        vim.cmd.normal({ "[c", bang = true })
+      else
+        gitsigns.nav_hunk("prev")
+      end
+    end, { desc = "Previous Git hunk" })
+
+    -- Actions
+    map("n", "<leader>ghs", gitsigns.stage_hunk, { desc = "Stage hunk" })
+    map("n", "<leader>ghr", gitsigns.reset_hunk, { desc = "Reset hunk" })
+
+    map("v", "<leader>ghs", function()
+      gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+    end, { desc = "Stage selected hunk" })
+
+    map("v", "<leader>ghr", function()
+      gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+    end, { desc = "Reset selected hunk" })
+
+    map("n", "<leader>ghS", gitsigns.stage_buffer, { desc = "Stage buffer" })
+    map("n", "<leader>ghR", gitsigns.reset_buffer, { desc = "Reset buffer" })
+
+    map("n", "<leader>ghp", gitsigns.preview_hunk, { desc = "Preview hunk" })
+    map("n", "<leader>ghi", gitsigns.preview_hunk_inline, { desc = "Preview hunk inline" })
+
+    map("n", "<leader>ghb", function()
+      gitsigns.blame_line({ full = true })
+    end, { desc = "Blame current line" })
+
+    map("n", "<leader>ghd", gitsigns.diffthis, { desc = "Diff current file" })
+
+    map("n", "<leader>ghD", function()
+      gitsigns.diffthis("~")
+    end, { desc = "Diff against previous commit" })
+
+    map("n", "<leader>ghQ", function()
+      gitsigns.setqflist("all")
+    end, { desc = "Git hunks → quickfix (all)" })
+
+    map("n", "<leader>ghq", gitsigns.setqflist, { desc = "Git hunks → quickfix" })
+
+    -- Toggles
+    map("n", "<leader>gtb", gitsigns.toggle_current_line_blame, {
+      desc = "Toggle Git blame",
+    })
+
+    map("n", "<leader>gtw", gitsigns.toggle_word_diff, {
+      desc = "Toggle word diff",
+    })
+
+    -- Text object
+    map({ "o", "x" }, "ih", gitsigns.select_hunk, {
+      desc = "Select Git hunk",
+    })
+  end,
 })
